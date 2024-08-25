7368c3216bd0f73f4251086824dde50f7abdef9a
Poojahh <poojakrishnan1104@gmail.com>
2024-08-25 21:11:21 +0530

    User App Api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddUsers from './components/AddUsers';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SearchUsers from './components/SearchUsers';
+import ViewUsers from './components/ViewUsers';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AddUsers/>}/>
         <Route path="/search" element={<SearchUsers/>}/>
+        <Route path="/view" element={<ViewUsers/>}/>
       </Routes>
       </BrowserRouter>
     </div>
